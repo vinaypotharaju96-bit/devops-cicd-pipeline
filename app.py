5234f937bbeb5f53386d326513b3043bc5a1f5f8
vinaypotharaju96-bit <vinaypotharaju96@gmail.com>
2025-12-13 21:31:08 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "CI/CD Pipeline is Working!"
+def hello():
+    return "Hello from Flask App 🚀 VERSION v2 (GitOps + Argo CD)"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
