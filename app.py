72a702926a4ba2a93cbc2a3b4a5e535662d6325c
vinaypotharaju96-bit <vinaypotharaju96@gmail.com>
2025-12-13 21:46:59 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Flask App 🚀 Version 2"
+    return "Hello from Flask App 🚀 Version v2"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=5000)
