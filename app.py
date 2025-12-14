b0f3b0f78878762c9a1ea5e13930eb262f0aa60a
vinaypotharaju96-bit <vinaypotharaju96@gmail.com>
2025-12-13 21:36:20 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Flask App 🚀 VERSION v2 (GitOps + Argo CD)"
+    return "Hello from Flask App 🚀 Version 2"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=80)
